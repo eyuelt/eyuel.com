ca1f310a6cce06863d0de0cdd82679108cd69904
eyuelt <eyueltessema@gmail.com>
2013-07-06 05:21:19 +0000

    Add placeholder frontpage and create resources directory

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 app.get('/', function(req, res) {
-  var body = 'Hello, world!\n';
+  var body = '<html><head><title>Hello</title></head><body>';
+  body += '<center><img src="/cookies" /></center>';
+  body += '</body></html>';
   res.send(body);
 });
 
@@ -10,6 +13,12 @@ app.get('/error', function(req, res) {
   console.log("Throwing error");
   console.log(req.url);
   throw "oops thats an error";
+});
+
+app.get('/cookies', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'image/jpg'});
+  var img = fs.readFileSync('./resources/cookies.jpg');
+  res.end(img, 'binary');
 });
 
 app.listen(8080);
