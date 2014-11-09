9e45ac9df4c70c77f3d4234ebfd94246310bcc5a
eyuelt <eyueltessema@gmail.com>
2014-11-08 16:34:45 -0800

    Add stuff to gulpfile. Change dir structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,40 @@
 var gulp = require('gulp');
+var server = require('./server');
+var del = require('del');
 
-gulp.task('default', function() {
-  console.log("Using gulp");
+//Clean
+gulp.task('clean:styles', function() {
+    del(['dist/styles/**/*']);
 });
+gulp.task('clean:scripts', function() {
+    del(['dist/scripts/**/*']);
+});
+gulp.task('clean:images', function() {
+    del(['dist/images/**/*']);
+});
+gulp.task('clean', ['clean:styles', 'clean:scripts', 'clean:images']);
+
+
+//Build
+gulp.task('build:styles', ['clean:styles'], function() {
+    return gulp.src('src/styles/**/*.css')
+    .pipe(gulp.dest('dist/styles/'));
+});
+gulp.task('build:scripts', ['clean:scripts'], function() {
+    return gulp.src('src/scripts/**/*.js')
+    .pipe(gulp.dest('dist/scripts/'));
+});
+gulp.task('build:images', ['clean:images'], function() {
+    return gulp.src('src/images/**/*')
+    .pipe(gulp.dest('dist/images/'));
+});
+gulp.task('build', ['build:styles', 'build:scripts', 'build:images']);
+
+
+gulp.task('serve', ['build'], function() {
+    server.serve(function() {
+        //open browser
+    });
+});
+
+gulp.task('default', ['build']);
--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 var express = require('express');
-var routes = require('./src/routes/index');
+var routes = require('./routes/index');
 var app = module.exports = express();
 
 //config
 app.set('port', process.env.PORT || 8080);
-app.set('views', __dirname + '/src/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use('/static', express.static(__dirname + '/src/public'));
+app.set('env', process.env.NODE_ENV || 'dev');
+
+//static content
+app.use('/static', express.static(__dirname + '/dist'));
 
 //routes
 app.get('/', routes.homepage);
 app.get('*', routes.notfound);
 
 //start server
-app.listen(app.get('port'), function() {
-  console.log('Server listening on port ' + app.get('port'));
-});
+module.exports.serve = function(cb) {
+    app.listen(app.get('port'), function() {
+        console.log('[' + app.get('env') + '] Server listening on port ' + app.get('port'));
+        if (cb) cb();
+    });
+}
+
+//only start server if called from command line
+if (require.main === module) {
+    app.serve();
+}
